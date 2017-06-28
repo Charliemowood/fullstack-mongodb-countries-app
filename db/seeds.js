d88b05798d64d8ddf8aca7b8455537fd86694a13
Charlie Wood <charliemowood@gmail.com>
2017-06-28 14:34:46 +0100

    Create database and seeds

---
+++ db/seeds.js
@@ -0,0 +1,7 @@
+use countries_db;
+
+db.countries.insert([
+  {name: "Germany", capital: "Berlin", reason: "to eat german sausages and wear Lederhosen"},
+{name: "Canada", capital: "Ottawa", reason: "to visit Vancouver and hiking"},
+ {name: "Burkina-Faso", capital: "Ouagadougou", reason: "to eat some bread"}
+]);
